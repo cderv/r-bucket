4706a3247d0f81cf3ddee08f42776a124da185e8
Christophe Dervieux <christophe.dervieux@gmail.com>
2026-01-19 10:21:50 +0100

    Use correct repo for auto-pr script

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ScoopInstaller/Main:master"
+    [String]$upstream = "cderv/r-bucket:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
